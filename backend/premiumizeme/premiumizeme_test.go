ce2d738653f52f500d4be0bf3d048628fc6b6cc7
Nick Craig-Wood <nick@craig-wood.com>
2019-07-06 00:16:57 +0100

    premiumizeme: new backend for premiumize.me - FIXME WIP
    Done
    - code
    - integration tests
    
    Todo
    - docs
    
    Fixes #3063

---
+++ backend/premiumizeme/premiumizeme_test.go
@@ -0,0 +1,17 @@
+// Test Box filesystem interface
+package premiumizeme_test
+
+import (
+	"testing"
+
+	"github.com/ncw/rclone/backend/premiumizeme"
+	"github.com/ncw/rclone/fstest/fstests"
+)
+
+// TestIntegration runs integration tests against the remote
+func TestIntegration(t *testing.T) {
+	fstests.Run(t, &fstests.Opt{
+		RemoteName: "TestPremiumizeMe:",
+		NilObject:  (*premiumizeme.Object)(nil),
+	})
+}
